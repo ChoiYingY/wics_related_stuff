1fd1f6085c843717e0a49635c80e3db8f2d2e406
ChoiYingY <ycying1226@gmail.com>
2023-10-31 17:38:28 -0400

    created script to find active freshmen, updated readme

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 Purpose of each python files:
 <ol>
     <li>search_subscribers.py is for reading csv to find new subscribers who answered 'yes' when asked to subscribe WiCS newsletter</li>
-    <li>wics_voters.py is for keeping track of each WiCS member's attendance in every WiCS event</li>
+    <li>wics_voters_2324.py is for keeping track of each WiCS member's attendance in every WiCS event in 2023-2024</li>
     <li>event_attendance.py is for keeping track of total attendance of each WiCS event</li>
+    <li>count_members.py is for counting all general body members & active members in each year.</li>
+    <li>active_members.py is for keeping track of all WiCS active members who have attended at least N events</li>
+    <li>active_freshmen.py is for keeping track of all WiCS active members who are freshmen & have attended at least N events</li>
 </ol>
 
 # Usage menu
 Usage: python3 search_subscribers.py <event_response_csv>
-<br>Usage: python3 wics_voters.py <attendance_responses_directory> <output_filename>
+<br>Usage: python3 wics_voters_2324.py <attendance_responses_directory> <output_attendance_sheet_filename>
 <br>Usage: python3 event_attendance.py <attendance_sheet>
+<br>Usage: python3 count_members.py <attendance_sheet> <min_num_events_attended>
+<br>Usage: python3 active_members.py <attendance_sheet> <min_num_events_attended>
+<br>Usage: python3 active_freshmen.py <attendance_sheet> <min_num_events_attended>
+
+# Example
---
+++ active_freshmen.py
@@ -0,0 +1,49 @@
+import sys
+import pandas as pd
+
+'''
+    Helper function to find name & email of all active members who are freshmen
+'''
+def find_all_active_freshmen(attendance_sheet, min_num_events_attended):
+    df = pd.read_excel(attendance_sheet, sheet_name='Active Roster')
+    
+    # filter out members that have attended less than N events & are not freshmen
+    df = df[df['Total #events attended'] >= min_num_events_attended]
+    df = df[df['Year'] == 'Freshman']
+
+    # Only keep email & name columns (+ year col for temporary use) & rename the columns
+    columns_to_keep = ['Email', 'Name', 'Year']
+    df = df[columns_to_keep]
+
+    # Set email column
+    df['Email Address'] = df['Email']
+
+    # Set first&last name column, handle case fo people who have two words in first name
+    first_last_name = df['Name'].str.split()
+    df['First Name'] = first_last_name.str[:-1].str.join(' ')
+    df['Last Name'] = first_last_name.str[-1]
+
+    # Drop unwanted columns & preserve Email Address,First Name,Last Name columns
+    df.drop(columns=['Name', 'Email', 'Year'], inplace=True)
+
+    # export dataframe as csv file
+    df.to_csv('Active Members (Freshmen only).csv', index=False)
+
+    
+'''
+    Main function to find all active members from given excel sheet & export to csv
+'''
+if __name__== '__main__':
+    try:
+        if len(sys.argv) != 3:
+            raise Exception('Usage: python3 active_freshmen.py <attendance_sheet> <min_num_events_attended>')
+        else:
+            attendance_sheet = sys.argv[1].strip()
+            min_num_events_attended = int(sys.argv[2].strip())
+
+            # find email & name for all active freshmen & export to csv
+            active_freshmen = find_all_active_freshmen(attendance_sheet, min_num_events_attended)
+
+    except Exception as e:
+        print(f'ERROR: {e}')
+        sys.exit()
